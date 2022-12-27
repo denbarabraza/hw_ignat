9a04570ccf8aed25e870a4aa4f47a6f66154f54d
denbarabraza <denis.bareischev@gmail.com>
2022-12-28 00:15:22 +0300

    Hometask №12up

--- src/s2-homeworks/hw12/HW12.tsx
+++ src/s2-homeworks/hw12/HW12.tsx
@@ -15,9 +15,9 @@ import {changeThemeId} from "./bll/themeReducer";
 
 
 const themes = [
-    {id: 1, value: 'Light'},
-    {id: 2, value: 'Blue'},
-    {id: 3, value: 'Dark'},
+    {id: 1, value: 'light'},
+    {id: 2, value: 'blue'},
+    {id: 3, value: 'dark'},
 ]
 
 const HW12 = () => {
