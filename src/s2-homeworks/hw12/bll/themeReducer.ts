630d928a3b3feef5b10401348314e304eb0a9519
denbarabraza <denis.bareischev@gmail.com>
2022-12-27 23:12:15 +0300

    Hometask №12up

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,11 +1,11 @@
-const initState = {
-    themeId: 1
-}
-
 export type ThemeType = {
     themeId: number
 }
+export type ActionsType = ReturnType<typeof changeThemeId>
 
+const initState: ThemeType = {
+    themeId: 1,
+}
 
 export const themeReducer = (state = initState, action: ActionsType): ThemeType => {
     debugger
@@ -14,12 +14,17 @@ export const themeReducer = (state = initState, action: ActionsType): ThemeType 
         case 'SET_THEME_ID': {
             return {
                 ...state,
-                themeId: action.id}
+                themeId: action.id
+            }
         }
         default:
             return state
     }
 }
 
-export type ActionsType = ReturnType<typeof changeThemeId>
-export const changeThemeId = (id: number) => ({type: 'SET_THEME_ID', id} as const)
+export const changeThemeId = (id: number): changeThemeIdACType => ({type: 'SET_THEME_ID', id}) // fix any
+
+type changeThemeIdACType = {
+    type:  'SET_THEME_ID'
+    id: number
+}
